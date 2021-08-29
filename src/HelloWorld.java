650f6a618359489cde0f15f605d86eb2fa123be8
kuhan1209 <73043883+kuhan1209@users.noreply.github.com>
2021-08-29 15:26:00 +0200

    ola teller til tyve

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,17 @@ public class HelloWorld {
         System.out.println("Hello World");
 
         System.out.println("Teller til ti");
-        for (int i=0; i<10; ++i) {
+        for (int i=0; i<=10; ++i) {
             System.out.println(i);
         }
+
+
+        System.out.println("Ola teller til tyve");
+        for (int i=11; i<=20; ++i) {
+            System.out.println(i);
+        }
+
     }
 
-    
+
 }
